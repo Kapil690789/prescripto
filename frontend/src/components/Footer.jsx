2513dbfc80915db3f7cbe7849978d40f6ea32711
Kapil Sharma <kapil19092003@gmail.com>
2024-11-28 12:16:02 +0530

    Second

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { assets } from '../assets/assets';
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm">
       {/* Left Section */}
       <div>
         <img className="mb-5 w-40" src={assets.logo} alt="Logo" />
         <p className="w-full md:w-2/3 text-gray-600 leading-6">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          Prestcripto is a user-friendly web application designed to simplify
+          doctor appointment booking. Users can browse through a wide range of
+          healthcare providers, check availability, and book appointments
+          seamlessly. The platform allows users to filter doctors based on
+          specialization, location, and reviews. It also includes features for
+          managing medical history, reminders for appointments, and online
+          consultations. With a secure and intuitive interface, Prestcripto
+          bridges the gap between patients and healthcare professionals, making
+          healthcare more accessible. The app ensures efficient scheduling and
+          enhances the overall experience of managing medical needs.
         </p>
       </div>
 
@@ -18,10 +27,31 @@ function Footer() {
       <div>
         <p className="text-xl font-medium mb-5">COMPANY</p>
         <ul className="flex flex-col gap-2 text-gray-600">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact us</li>
-          <li>Privacy policy</li>
+          <li
+            onClick={() => {
+              navigate("/login");
+              scrollTo(0, 0);
+            }}
+          >
+            <button>Home</button>
+          </li>
+          <li
+            onClick={() => {
+              navigate("/about");
+            }}
+          >
+            <button>About</button>
+          </li>
+          <li
+            onClick={() => {
+              navigate("/contact");
+            }}
+          >
+            <button>Contact us</button>
+          </li>
+          <li onClick={()=> {
+            navigate('/doctors')
+          }}><button>Privacy policy</button></li>
         </ul>
       </div>
 
